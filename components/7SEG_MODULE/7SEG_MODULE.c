167aa44ff861f1dd4c53a7721c793111e90224b1
Majkii2006 <michaldoa08@gmail.com>
2026-09-22 20:31:42 +0200

    Dsiaply initialize function, main structures creation

--- components/7SEG_MODULE/7SEG_MODULE.c
+++ components/7SEG_MODULE/7SEG_MODULE.c
@@ -1,2 +1,47 @@
+#include <stdint.h>
+
 #include "7SEG_MODULE.h"
 
+#include "driver/gpio.h"
+
+#include "hal/gpio_types.h"
+
+static const uint8_t digitPatterns[] = {
+	0b00111111, // 0 its for the 0bGFEDCBA pattern, for default kathode controlled display, we can do it for anode via negation every bit
+	0b00000110, // 1
+	0b01011011, // 2
+	0b01001111, // 3
+	0b01100110, // 4
+	0b01101101, // 5
+	0b01111101, // 6
+        0b00000111, // 7
+	0b01111111, // 8
+	0b01101111  // 9
+};
+
+
+
+
+void display_init(SevenSegment_t* display, const uint8_t segPins[], uint8_t nSeg, const uint8_t digPins[], uint8_t nDig, bool isAno) {
+
+	//overfill protection	
+	if (nDig > MAX_DIGITS) { 
+		nDig = MAX_DIGITS;
+	}
+
+	display->segmentPins = segPins;
+	display->digitPins = digPins;
+	display->numDigits = nDig;
+	display->numSegments = nSeg;
+	display->isAnode = isAno;
+
+	//initialization as output
+	for (uint8_t i = 0; i < display->numSegments; i++) {
+		gpio_set_direction(display->segmentPins[i], GPIO_MODE_OUTPUT);
+	}	
+	for (uint8_t i = 0; i < display->numDigits; i++) {
+		gpio_set_direction(display->digitPins[i], GPIO_MODE_OUTPUT);
+		gpio_set_level(display->digitPins[i], display->isAnode ? 0 : 1);
+	}
+
+}
--- components/7SEG_MODULE/include/7SEG_MODULE.h
+++ components/7SEG_MODULE/include/7SEG_MODULE.h
@@ -1,11 +1,31 @@
+#pragma once
 
 #ifndef SEG_MODULE_H
 #define SEG_MODULE_H
 
+#define MAX_DIGITS 8 //for the compile-time memory allocating on the stack so we need to know how much of memory we need
+                     // CHANGE THIS VALUE FOR THE OPERATIONS ON MORE THAN 8 SEGMENTS/DIGITS 
+
+#include <stdint.h>
+
+typedef struct {
+	const uint8_t* segmentPins; // collection containing mapping segments of display to the each pin
+	const uint8_t* digitPins; // collection conating mapping numbers of display to the each pin
+	uint8_t numSegments; // how many segments for the one number -> default 7 (without DP) or 8 (with DP)
+	uint8_t numDigits; // how many digits have full display 
+	bool isAnode; // is the display controlled by shared anode or shared kathode 
+	uint8_t displayBuffer[MAX_DIGITS]; // the compile-time buffer for the concrete numbers from that main one that we would to show
+					   // for example -> 428 -> displayBuffer[0] = 4, displayBuffer[1] = 2, displayBuffer[2] = 8
+} SevenSegment_t;
+
+
+void display_init(SevenSegment_t* display, const uint8_t segPins[], uint8_t nSeg, const uint8_t digPins[], uint8_t nDig, bool isAno);
+void display_setNumber(SevenSegment_t* display, long number);
+void display_clear(SevenSegment_t* display);
+void display_refresh(SevenSegment_t* display);
 
 
 
 
 
-
-#endif /* ifndef 7SEG_MODULE_H */
+#endif /* ifndef SEG_MODULE_H */
